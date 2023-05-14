dc83ee74882ccf6e7f7a564ddc0b8eb559632886
Nikola Golubović <nikola.golubovic112@gmail.com>
2023-05-14 11:57:11 +0200

    update dbot.py

--- dbot.py
+++ dbot.py
@@ -12,18 +12,14 @@ else:
     token = sys.argv[1]
 print(f'Set token as {token}')
 
-join_greeting = '''Welcome to the DevOps Journey Discord! We are a new community that embraces everything DevOps related.
-
-#Resources Channel has plenty of free learning resources.
-
-#Devops Channel to spark up any devops related questions or conversations.
-
-YouTube Channel: https://www.youtube.com/channel/UC4Snw5yrSDMXys31I18U3gg
+join_greeting = '''
+Nikola Kapri Consulting
 '''
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
